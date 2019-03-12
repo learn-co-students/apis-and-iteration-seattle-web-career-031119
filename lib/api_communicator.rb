517448c24bcb46e715e14f038f6529b558f6fb74
github username <github email address>
2019-03-12 16:48:28 -0700

    cleanup and optimization. might be working? API crashed so not sure

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,6 +2,17 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+Numeral_lookup = {
+  1 => "I",
+  2 => "II",
+  3 => "III",
+  4 => "IV",
+  5 => "V",
+  6 => "VI",
+  7 => "VII",
+  8 => "VIII"
+  }
+
 def get_film_apis(character_name, response_hash)
   film_apis = []
   response_hash["results"].each do |character_hash|
@@ -12,9 +23,10 @@ def get_film_apis(character_name, response_hash)
   film_apis
 end
 
-def get_movie_array(film_apis)
+def get_movie_array(movie_urls)
+
   movie_arr = []
-  film_apis.each do |movie_url|
+  movie_urls.each do |movie_url|
     film_string = RestClient.get(movie_url)
     film_hash = JSON.parse(film_string)
     movie_arr << film_hash
@@ -22,55 +34,49 @@ def get_movie_array(film_apis)
   movie_arr
 end
 
+
 def get_character_movies_from_api(character_name)
-  #make the web request
 
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-
-  film_apis = get_film_apis(character_name, response_hash)
+  url = 'http://www.swapi.co/api/people/'
+  film_apis = {}
 
   while film_apis.empty?
-    response_string = RestClient.get(response_hash["next"])
+    response_string = RestClient.get(url)
     response_hash = JSON.parse(response_string)
     film_apis = get_film_apis(character_name, response_hash)
+    url = response_hash["next"]
+
+    if !response_hash.has_key?("next") && film_apis.empty?
+      puts "That character is not in Star Wars, you dolt."
+      break
+    end
+
   end
-
   get_movie_array(film_apis)
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
-def create_movie_hash(movie_arr)
-    movie_hash = {}
-    movie_arr.each { |film|
-      movie_hash[film["episode_id"]] = {"title" => film["title"], "year" => film["release_date"][0..3]}
-      }
-    movie_hash
+def create_movie_hash(films)
+  #so we can put stuff in order
+  movie_hash = {}
+  films.each do |film|
+    movie_hash[film["episode_id"]] = {"title" => film["title"], "year" => film["release_date"][0..3]}
+  end
+  movie_hash
 end
 
 def print_movies(films)
   movie_hash = create_movie_hash(films)
-  (1..8).each { |i|
-    if movie_hash.has_key?(i)
-      puts "Episode #{i}: #{movie_hash[i]["title"]} (#{movie_hash[i]["year"]})"
+  (1..8).each do |episode_num|
+    if movie_hash.has_key?(episode_num)
+      puts "Episode #{Numeral_lookup[episode_num]}: #{movie_hash[episode_num]["title"]} (#{movie_hash[episode_num]["year"]})"
     end
-  }
-
-  # some iteration magic and puts out the movies in a nice list
+  end
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
+  true
 end
 
 ## BONUS
