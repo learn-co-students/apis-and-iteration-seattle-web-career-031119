513f71ebf088bf693ab9328642dee6a4a503b97c
github username <github email address>
2019-03-12 13:16:15 -0700

    doug's contribution

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,10 +2,34 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+def get_film_apis(character_name, response_hash)
+  film_apis = []
+  response_hash["results"].each do |character_hash|
+    if character_hash["name"].downcase == character_name
+      film_apis = character_hash["films"]
+    end
+  end
+  film_apis
+end
+
+def get_movie_array(film_apis)
+  movie_arr = []
+  film_apis.each do |movie_url|
+    film_string = RestClient.get(movie_url)
+    film_hash = JSON.parse(film_string)
+    movie_arr << film_hash
+  end
+  movie_arr
+end
+
 def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+
+  film_apis = get_film_apis(character_name, response_hash)
+  movie_array = get_movie_array(film_apis)
+  binding.pry
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to STAR WARS character's movie search feature!"
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  gets.strip.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
