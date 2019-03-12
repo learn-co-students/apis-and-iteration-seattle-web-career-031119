dab674a0e79d9d5fddd39b23cf7a8dea2e30195c
quinncidences <qrc.inpa@gmail.com>
2019-03-12 12:53:56 -0700

    rough draft of star wars

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,6 +6,22 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  character_data = {}
+  while character_data == {}
+    response_hash["results"].each {|character_info|
+      if character_info["name"].downcase == character_name
+        character_data = character_info
+      end
+    }
+    response_string = RestClient.get(response_hash["next"])
+    response_hash = JSON.parse(response_string)
+  end
+    films = character_data["films"]
+      film_titles = films.map { |movie_url|
+        movie_response_string = RestClient.get(movie_url)
+        movie_response_hash = JSON.parse(movie_response_string)
+          movie_response_hash["title"]
+      }
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -19,7 +35,9 @@ def get_character_movies_from_api(character_name)
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  films.each {|film_name|
+      puts film_name
+    }
 end
 
 def show_character_movies(character)
