6ce3662ae05300a633596ec075f54d5933a35886
jyang81 <joe.yang.design@gmail.com>
2019-03-12 16:53:46 -0700

    first draft of code

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,24 +2,53 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+def get_character_movies_from_api(character_name)
+  response_string = RestClient.get("http://www.swapi.co/api/people/")
+  response_hash = JSON.parse(response_string)
+  page_count = (response_hash["count"]/10.0).ceil
+  films_array = []
+  page = 1
+  while page <= page_count
+    response_string = RestClient.get("http://www.swapi.co/api/people/")
+    if page > 1
+      response_string = RestClient.get(response_hash["next"])
+    end
+    response_hash = JSON.parse(response_string)
+    character_array = response_hash["results"]
+    films_array = search_page(character_array, character_name)
+    if films_array != []
+      page = page_count + 1
+    else
+      page += 1
+    end
+  end
+  films_array
+end
+
+def search_page(character_array, character_name)
+  films_array ||= []
+  character_array.each do |character_hash|
+    if character_hash["name"].downcase == character_name
+      films_array = character_hash["films"]
+    end
+  end
+  films_array
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  title_array = []
+  films.each do |film|
+    response_string = RestClient.get(film)
+    response_hash = JSON.parse(response_string)
+    title_array << response_hash["title"]
+  end
+  if title_array == []
+    puts "Character not found."
+  else
+    puts title_array
+  end
+  #binding.pry
 end
 
 def show_character_movies(character)
