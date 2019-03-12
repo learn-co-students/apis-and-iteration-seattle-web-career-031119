30ec28aee4eedb3d6a9c7e32370e704ea01cb1cb
quinncidences <qrc.inpa@gmail.com>
2019-03-12 14:07:39 -0700

    optimized star wars

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,35 +4,55 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-  character_data = {}
-  while character_data == {}
+    character_data = get_character_data(character_name)
+    if character_data == {}
+      puts "That character doesn't appear in any movies! Please add the name of a Star Wars character."
+    else
+      films = get_films(character_data)
+      film_titles = get_film_titles(films)
+    end
+end
+
+
+def get_character_data(character_name)
+  response_hash = get_next_page()
+  loop do
     response_hash["results"].each {|character_info|
       if character_info["name"].downcase == character_name
-        character_data = character_info
+        return character_info
+        break
       end
     }
-    response_string = RestClient.get(response_hash["next"])
-    response_hash = JSON.parse(response_string)
+    next_page?(response_hash) ? response_hash = get_next_page(response_hash["next"]) : break
   end
-    films = character_data["films"]
-      film_titles = films.map { |movie_url|
-        movie_response_string = RestClient.get(movie_url)
-        movie_response_hash = JSON.parse(movie_response_string)
-          movie_response_hash["title"]
-      }
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+    {}
 end
+
+
+def get_films(data)
+  data["films"]
+end
+
+
+def get_film_titles(films)
+  films.map { |movie_url|
+    movie_response_string = RestClient.get(movie_url)
+    movie_response_hash = JSON.parse(movie_response_string)
+    movie_response_hash["title"]
+  }
+end
+
+
+def next_page?(page)
+  return !page["next"].nil?
+end
+
+
+def get_next_page(url = 'http://www.swapi.co/api/people/')
+  response_string = RestClient.get(url)
+  response_hash = JSON.parse(response_string)
+end
+
 
 def print_movies(films)
   films.each {|film_name|
@@ -40,9 +60,10 @@ def print_movies(films)
     }
 end
 
+
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
-  print_movies(films)
+  print_movies(films) if films
 end
 
 ## BONUS
